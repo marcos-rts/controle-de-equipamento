ceb2c50cf3e706c62090baa832264161613df232
Marcos Alexandre <reis.marcos43@gmail.com>
2022-09-29 12:14:01 -0300

    implementação do modo de paginação

--- sistem/entregues.php
+++ sistem/entregues.php
@@ -24,64 +24,67 @@ if (!isset($_SESSION['UsuarioID'])) {
     <title>Página Inicial</title>
 </head>
 <header>
-<div class="container">
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="../index.php">Sistema de Controle de Maquinas</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#conteudoNavbarSuportado" aria-controls="conteudoNavbarSuportado" aria-expanded="false" aria-label="Alterna navegação">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="conteudoNavbarSuportado">
-    <ul class="navbar-nav ml-auto mr-md-3">
-      <li class="nav-item active">
-        <a class="nav-link" href="../index.php">Home <span class="sr-only">(página atual)</span></a>
-      </li>
-      <li class="nav-item">
-       <!--  <a class="nav-link" href="#">Link</a> -->
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Maquinas
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="../index.php">Pendentes</a>
-          <a class="dropdown-item" href="../sistem/transferencia.php">Transferencia</a>
-          <a class="dropdown-item" href="../sistem/entregues.php">Entregues</a>
-          <a class="dropdown-item" href="../sistem/maquinas_livres.php">Livres</a>
-          <a class="dropdown-item" href="#"></a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item disabled" href="#">Em Breve</a>
-        </div>
-      </li>
+    <div class="container">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="../index.php">Sistema de Controle de Maquinas</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#conteudoNavbarSuportado"
+                aria-controls="conteudoNavbarSuportado" aria-expanded="false" aria-label="Alterna navegação">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="conteudoNavbarSuportado">
+                <ul class="navbar-nav ml-auto mr-md-3">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="../index.php">Home <span class="sr-only">(página atual)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <!--  <a class="nav-link" href="#">Link</a> -->
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Maquinas
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="../index.php">Pendentes</a>
+                            <a class="dropdown-item" href="../sistem/transferencia.php">Transferencia</a>
+                            <a class="dropdown-item" href="../sistem/entregues.php">Entregues</a>
+                            <a class="dropdown-item" href="../sistem/maquinas_livres.php">Livres</a>
+                            <a class="dropdown-item" href="#"></a>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item disabled" href="#">Em Breve</a>
+                        </div>
+                    </li>
 
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-         <?php echo $_SESSION['UsuarioNome'] ?>
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-           <a class="dropdown-item" href="../users/index.php">Informação</a>
-          <a class="dropdown-item" href="../users/trocarsenha.php">Trocar Senha</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="../logout.php">Logout</a>
-          <?php
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <?php echo $_SESSION['UsuarioNome'] ?>
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="../users/index.php">Informação</a>
+                            <a class="dropdown-item" href="../users/trocarsenha.php">Trocar Senha</a>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item" href="../logout.php">Logout</a>
+                            <?php
 	  if ($_SESSION['UsuarioNivel'] == '5' || $_SESSION['UsuarioID'] == '7') {
           ?>
-          <a href="./private/config.php" class="dropdown-item">Configurações gerais</a>
-          <?php
+                            <a href="./private/config.php" class="dropdown-item">Configurações gerais</a>
+                            <?php
           }
           ?>
-        </div>
-      </li>
-      <li class="nav-item">
-       <!--  <a class="nav-link disabled" href="#">Desativado</a> -->
-      </li>
-    </ul>
-  </div>
-</nav>
-</div>
+                        </div>
+                    </li>
+                    <li class="nav-item">
+                        <!--  <a class="nav-link disabled" href="#">Desativado</a> -->
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </div>
 </header>
 
 <body>
-   <div class="container">
+    <div class="container">
         <!-- <div class="jumbotron">
             <p>Conectado como <?php echo $_SESSION['UsuarioNome']; ?> </p>
 
@@ -147,7 +150,7 @@ if (!isset($_SESSION['UsuarioID'])) {
         <?php
         if ($_SESSION['UsuarioNivel'] == '2') {
         ?>
-<h1>ENTREGUES</h1>
+        <h1>ENTREGUES</h1>
         <div class="row">
             <br>
             <table class="table table-striped">
@@ -158,7 +161,7 @@ if (!isset($_SESSION['UsuarioID'])) {
                         <th scope="col">Local</th>
                         <th scope="col">Setor</th>
                         <th scope="col">Saida</th>
-                     <!--   <th scope="col">Status</th> -->
+                        <!--   <th scope="col">Status</th> -->
                         <th scope="col">Chapa</th>
                         <th scope="col">ID</th>
                         <th scope="col">Ação</th>
@@ -166,11 +169,37 @@ if (!isset($_SESSION['UsuarioID'])) {
                 </thead>
                 <tbody>
                     <?php
-                        include 'banco.php';
-                        $pdo = Banco::conectar();
-                        $sql = 'SELECT * FROM maquina ORDER BY id DESC';
+$pagina = (isset($_GET['pagina']))? $_GET ['pagina'] : 1;
+include 'banco.php';
+$pdo = Banco::conectar();
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+// selecionar todos itens da tabela
+$sql = 'SELECT * FROM maquina';
 
-                        foreach ($pdo->query($sql) as $row) {
+// contar total de dados
+$total = 0;
+
+$sql = $pdo->query($sql);
+$total = $sql->rowCount();
+// $total = mysqli_num_rows($sql);
+
+// Setar a quantidade de dados por pagina
+$quantidade_pg = 12;
+
+// calcular o numero de paginas necessarias para paresentar os dados 
+$num_pagina = ceil($total/$quantidade_pg);
+
+// calcular o inicio da visualização 
+$inicio = ($quantidade_pg*$pagina)-$quantidade_pg;
+
+
+// selecionar os itens a serem apresentados na pagina
+
+$result = 'SELECT * FROM maquina LIMIT '.$inicio.', '. $quantidade_pg;
+
+$result2 = $result;
+
+                        foreach ($pdo->query($result2) as $row) {
                             if ($row['status'] == 'Entregue') {
                                 echo '<tr>';
                                 echo '<th scope="row">' . $row['equip'] . '</th>';
@@ -191,6 +220,58 @@ if (!isset($_SESSION['UsuarioID'])) {
                         ?>
                 </tbody>
             </table>
+            <?php
+                // verirficar pagina anterior e posterior
+                $pagina_anterior = $pagina - 1;
+                $pagina_posterior = $pagina + 1;
+                ?>
+            <nav aria-label="Navegação de página exemplo" class="text-center">
+                <ul class="pagination">
+                    <li class="page-item">
+                        <?php
+                        if($pagina_anterior !=0){
+                        ?>
+                        <a class="page-link" href="entregues.php?pagina=<?php echo $pagina_anterior ?>"
+                            aria-label="Anterior">
+                            <span aria-hidden="true">&laquo;</span>
+                            <span class="sr-only">Anterior</span>
+                        </a>
+                        <?php
+                        }
+                        ?>
+
+                    </li>
+                    <li class="page-item"><a class="page-link" href="entregues.php?pagina=1">1</a></li>
+                    <?php if ($pagina != 1 ) {?>
+                    <li class="page-item"><a class="page-link"
+                            href="entregues.php?pagina=<?php echo $pagina - 1 ?>"><?php echo $pagina - 1 ?></a></li>
+                    <li class="page-item"><a class="page-link"
+                            href="entregues.php?pagina=<?php echo $pagina ?>"><?php echo $pagina ?></a></li>
+                    <?php    # code...
+
+}?>
+
+                    <li class="page-item"><a class="page-link"
+                            href="entregues.php?pagina=<?php echo $pagina + 1 ?>"><?php echo $pagina + 1 ?></a></li>
+                    <li class="page-item"><a class="page-link"
+                            href="entregues.php?pagina=<?php echo $num_pagina ?>"><?php echo $num_pagina ?></a></li>
+
+                    <li class="page-item">
+                        <?php
+                        if($pagina_posterior <= $num_pagina){
+?>
+                        <a class="page-link" href="entregues.php?pagina=<?php echo $pagina_posterior ?>"
+                            aria-label="Próximo">
+                            <span aria-hidden="true">&raquo;</span>
+                            <span class="sr-only">Próximo</span>
+                        </a>
+                        <?php
+                        }
+                        ?>
+
+                    </li>
+                </ul>
+            </nav>
         </div>
         <?php
         }
@@ -317,7 +398,7 @@ if (!isset($_SESSION['UsuarioID'])) {
 
     <footer>
         <div class="container">
-            <span class="badge badge-secondary">v 1.0.2 &copy; 2021 - Marcos A. R. T. dos Santos</span>
+            <span class="badge badge-secondary">v 3.0.0 &copy; 2021 - Marcos A. R. T. dos Santos</span>
 
 
         </div>
