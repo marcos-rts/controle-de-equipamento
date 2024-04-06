c8cf7e06db977a4b0abf4ac2d6deb5475fdf5285
Marcos Alexandre <marcos.alex1002@gmail.com>
2024-04-06 17:20:18 -0300

    alteracao tela login

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php
-
 // A sessão precisa ser iniciada em cada página diferente
 if (!isset($_SESSION)) session_start();
 
@@ -24,44 +23,80 @@ if (!isset($_SESSION['UsuarioID'])) {
     <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <header>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <a class="navbar-brand" href="#">Navbar com texto</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#textoNavbar"
-            aria-controls="textoNavbar" aria-expanded="false" aria-label="Alterna navegação">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="textoNavbar">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
-                    <a class="nav-link" href="#">Home <span class="sr-only">(Página atual)</span></a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Destaques</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Preços</a>
-                </li>
-            </ul>
-            <ul class="navbar-nav">
-
-                <li class="nav-item">
-                    <a class="nav-link" href="index_bkp.php">Login</a>
-                </li>
-            </ul>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">Navbar</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Alterna navegação">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavDropdown">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(Página atual)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Destaques</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Preços</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Link dropdown
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          <a class="dropdown-item" href="#">Ação</a>
+          <a class="dropdown-item" href="#">Outra ação</a>
+          <a class="dropdown-item" href="#">Algo mais aqui</a>
         </div>
-    </nav>
+      </li>
+    </ul>
+  </div>
+</nav>
 </header>
 
-<body class="img js-fullheight" style="background-image: url(img/wallpaper.jpg);">
-<h1>Pesquisar Cursos</h1>
-<form method="POST" action="sistem/pesquisar.php">
-    Pesquisar:<input type="text" name="pesquisar" placeholder="PESQUISAR">
-    <input type="submit" value="ENVIAR">
-</form>
-    <script src="assets/js/jquery.min.js"></script>
-    <script src="assets/js/popper.js"></script>
-    <script src="assets/js/bootstrap.min.js"></script>
-    <script src="assets/js/main.js"></script>
-</body>
 
+<body>
+    <main role="main">
+    <div class="jumbotron">
+  <h1 class="display-4">Olá, mundo!</h1>
+  <p class="lead">Este é um simples componente jumbotron para chamar mais atenção a um determinado conteúdo ou informação.</p>
+  <hr class="my-4">
+  <p>Ele usa classes utilitárias para tipografia e espaçamento de conteúdo, dentro do maior container.</p>
+  <a class="btn btn-secondary btn-lg" href="#" role="button">Leia mais</a>
+</div>
+<div class="container">
+
+<div class="card-deck">
+  <div class="card shadow p-3 mb-5 bg-white rounded">
+    <!-- <img class="card-img-top" src=".../100px200/" alt="Imagem de capa do card"> -->
+    <div class="card-body">
+      <h5 class="card-title">Título do card</h5>
+      <p class="card-text">Este é um card mais longo com suporte a texto embaixo, que funciona como uma introdução a um conteúdo adicional. Este conteúdo é um pouco maior.</p>
+      <!-- <p class="card-text"><small class="text-muted">Atualizados 3 minutos atrás</small></p> -->
+    </div>
+  </div>
+  <div class="card shadow p-3 mb-5 bg-white rounded">
+    <!-- <img class="card-img-top" src=".../100px200/" alt="Imagem de capa do card"> -->
+    <div class="card-body">
+      <h5 class="card-title">Título do card</h5>
+      <p class="card-text">Este é um card com suporte a texto embaixo, que funciona como uma introdução a um conteúdo adicional.</p>
+      <!-- <p class="card-text"><small class="text-muted">Atualizados 3 minutos atrás</small></p> -->
+    </div>
+  </div>
+  <div class="card shadow p-3 mb-5 bg-white rounded">
+    <!-- <img class="card-img-top" src=".../100px200/" alt="Imagem de capa do card"> -->
+    <div class="card-body ">
+      <h5 class="card-title">Título do card</h5>
+      <p class="card-text">Este é um card maior com suporte a texto embaixo, que funciona como uma introdução a um conteúdo adicional. Este card tem o conteúdo ainda maior que o primeiro, para mostrar a altura igual, em ação.</p>
+      <!-- <p class="card-text"><small class="text-muted">Atualizados 3 minutos atrás</small></p> -->
+    </div>
+  </div>
+</div>
+</main>
+</div>
+</body>
+    <blockquote class="blockquote text-center">
+        <p class="mb-0">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum tempora nihil magnam id ullam soluta commodi. Numquam ipsum pariatur consectetur animi et recusandae maxime laudantium perferendis quisquam soluta. Molestias, minima!</p>
+        <footer class="blockquote-foter">Teste</footer>
+</blockquote>
 </html>
--- validacao.php
+++ validacao.php
@@ -1,28 +1,42 @@
 <?php
-
 // Verifica se houve POST e se o usuário ou a senha é(são) vazio(s)
 if (!empty($_POST) AND (empty($_POST['usuario']) OR empty($_POST['senha']))) {
     header("Location: index.php"); exit;
 }
-
-
-
 
 $servidor_bd = "192.168.0.110:3306";
 $usuario_bd = "SistemMaquina";
 $senha_bd = "hoome2024";
 $banco_bd = "Controle_de_Sistema";
 
-$conn = mysqli_connect($servidor_bd, $usuario_bd, $senha_bd, $banco_bd);
+$conn = mysqli_connect($servidor_bd, $usuario_bd, $senha_bd, $banco_bd) or trigger_error(mysql_error());
 
+$usuario = $_POST['usuario'];
+$senha = $_POST['senha'];
 
+$sql = "SELECT id, nome, nivel 
+        FROM usuarios
+        WHERE (usuario = '". $usuario ."') 
+        AND (senha = SHA1('". $senha ."')) 
+        AND (ativo = 1) 
+        LIMIT 1";
 
+$query = mysqli_query($conn, $sql);
+if (mysqli_num_rows($query) != 1){
+    //Mensagem de erro quando os dados sao invalidos e/ou o usuario nao foi encontrado
+    echo "Login Invalido!"; exit;
+}else{
+    // Salva os dados encontados na variável $resultado
+    $resultado = mysqli_fetch_assoc($query);
+    // Se a sessão não existir, inicia uma
+      if (!isset($_SESSION)) session_start();
 
+      // // Salva os dados encontrados na sessão
+      $_SESSION['UsuarioID'] = $resultado['id'];
+      $_SESSION['UsuarioNome'] = $resultado['nome'];
+      $_SESSION['UsuarioNivel'] = $resultado['nivel'];
 
-echo "<h1>Redirecionamento OK .2</h1>";
-
-
-
-
-
+      // Redireciona o visitante
+      header("Location: sistem/index.php"); exit;
+}
 ?>
